d906cdb6664e896ad7bc432f1dfc34a06fc8eafc
NikPeg <peganov.nik@gmail.com>
2022-05-02 19:49:32 +0300

    Add links in table

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ from random import randint
 import csv
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+category_id = "posuda-dlya-prigotovleniya-19005"
 driver.get(
-    "https://www.ozon.ru/category/posuda-dlya-prigotovleniya-19005/?price=79.000%3B1000.000&reviews_count=1.000%3B200.000")
+    f"https://www.ozon.ru/category/{category_id}/?price=79.000%3B1000.000&reviews_count=1.000%3B200.000")
 WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
-time.sleep(random())
-time.sleep(1)
+time.sleep(1 + random())
 csv_file = open('ozon.csv', 'w', newline='', encoding='utf-8')
 csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 while True:
     try:
         for card in driver.find_elements(By.CLASS_NAME, "i3p"):
             price = card.find_element(By.CLASS_NAME, "ui-s2").find_element(By.CLASS_NAME, "ui-s5").text
-            hyper_link = card.find_element(By.CLASS_NAME, "tile-hover-target").text
+            link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
             name = card.find_element(By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
             reviews = card.find_element(By.CLASS_NAME, "ni6").find_element(By.TAG_NAME, "a").text
-            csv_writer.writerow([name, str(price), reviews])
+            csv_writer.writerow([name, str(price), reviews, link])
         item = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
         item.click()
         WebDriverWait(driver, 3).until(
