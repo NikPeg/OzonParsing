89e53f4faaee3e65069f06767773b04b2bbb9b4e
NikPeg <peganov.nik@gmail.com>
2022-05-02 22:21:30 +0300

    Add horizontal cards

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ from random import randint
 import csv
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-category_id = "posuda-dlya-prigotovleniya-19005"
+# category_id = "posuda-dlya-prigotovleniya-19005"
+category_id = "telefony-i-smart-chasy-15501"
 driver.get(
-    f"https://www.ozon.ru/category/{category_id}/?price=79.000%3B1000.000&reviews_count=1.000%3B200.000")
+    f"https://www.ozon.ru/category/{category_id}/?price=0.000%3B1000.000&reviews_count=0.000%3B200.000")
 WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
 time.sleep(1 + random())
-csv_file = open('ozon.csv', 'w', newline='', encoding='utf-8')
+csv_file = open(f'ozon/{category_id}.csv', 'w', newline='', encoding='utf-8')
 csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 while True:
     try:
@@ -27,6 +28,13 @@ while True:
             link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
             name = card.find_element(By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
             reviews = card.find_element(By.CLASS_NAME, "ni6").find_element(By.TAG_NAME, "a").text
+            csv_writer.writerow([name, str(price), reviews, link])
+        for card in driver.find_elements(By.CLASS_NAME, "p3i"):
+            price = card.find_element(By.CLASS_NAME, "p4i").find_element(By.CLASS_NAME, "ui-s5").text
+            link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
+            reviews = card.find_element(By.CLASS_NAME, "yc5").text
+            name = card.find_element(By.CLASS_NAME, "pi4").find_element(By.CLASS_NAME, "i5p").find_element(
+                By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
             csv_writer.writerow([name, str(price), reviews, link])
         item = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
         item.click()
