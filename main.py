6ad5a8f3935e85c0a034e03df8250e577a982c13
NikPeg <peganov.nik@gmail.com>
2022-05-02 22:56:01 +0300

    Add exceptions

--- main.py
+++ main.py
@@ -13,36 +13,57 @@ from random import randint
 import csv
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# category_id = "posuda-dlya-prigotovleniya-19005"
-category_id = "telefony-i-smart-chasy-15501"
-driver.get(
-    f"https://www.ozon.ru/category/{category_id}/?price=0.000%3B1000.000&reviews_count=0.000%3B200.000")
-WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
-time.sleep(1 + random())
-csv_file = open(f'ozon/{category_id}.csv', 'w', newline='', encoding='utf-8')
-csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-while True:
+categories_file = open("categories", "r")
+for category_id in categories_file:
+    category_id = category_id.strip()
     try:
-        for card in driver.find_elements(By.CLASS_NAME, "i3p"):
-            price = card.find_element(By.CLASS_NAME, "ui-s2").find_element(By.CLASS_NAME, "ui-s5").text
-            link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
-            name = card.find_element(By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
-            reviews = card.find_element(By.CLASS_NAME, "ni6").find_element(By.TAG_NAME, "a").text
-            csv_writer.writerow([name, str(price), reviews, link])
-        for card in driver.find_elements(By.CLASS_NAME, "p3i"):
-            price = card.find_element(By.CLASS_NAME, "p4i").find_element(By.CLASS_NAME, "ui-s5").text
-            link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
-            reviews = card.find_element(By.CLASS_NAME, "yc5").text
-            name = card.find_element(By.CLASS_NAME, "pi4").find_element(By.CLASS_NAME, "i5p").find_element(
-                By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
-            csv_writer.writerow([name, str(price), reviews, link])
-        item = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
-        item.click()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.presence_of_element_located((By.ID, "layoutPage")))
-        time.sleep(1 + random())
-        if randint(0, 10) == 0:
-            driver.execute_script(f"window.scrollBy(0,{randint(500, 1000)});")
-    except TimeoutException as e:
-        break
-csv_file.close()
+        driver.get(
+            f"https://www.ozon.ru/category/{category_id}/?price=0.000%3B1000.000&reviews_count=0.000%3B200.000")
+    except Exception as e:
+        print(f"Web-page opening: {e} in {category_id}")
+        continue
+    try:
+        csv_file = open(f'ozon/{category_id}.csv', 'w', newline='', encoding='utf-8')
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    except Exception as e:
+        print(f"File opening: {e} in {category_id}")
+        continue
+    while True:
+        try:
+            WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "y5s")))
+            if randint(0, 10) == 0:
+                time.sleep(0.5 + random())
+            if randint(0, 10) == 0:
+                driver.execute_script(f"window.scrollBy(0,{randint(500, 1000)});")
+        except Exception as e:
+            print(f"Waiting: {e} in {category_id}")
+            break
+        try:
+            for card in driver.find_elements(By.CLASS_NAME, "i3p"):
+                price = card.find_element(By.CLASS_NAME, "ui-s2").find_element(By.CLASS_NAME, "ui-s5").text
+                link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
+                name = card.find_element(By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
+                reviews = card.find_element(By.CLASS_NAME, "ni6").find_element(By.TAG_NAME, "a").text
+                csv_writer.writerow([name, str(price), reviews, link])
+            for card in driver.find_elements(By.CLASS_NAME, "p3i"):
+                price = card.find_element(By.CLASS_NAME, "p4i").find_element(By.CLASS_NAME, "ui-s5").text
+                link = card.find_element(By.CLASS_NAME, "tile-hover-target").get_attribute("href")
+                reviews = card.find_element(By.CLASS_NAME, "yc5").text
+                name = card.find_element(By.CLASS_NAME, "pi4").find_element(By.CLASS_NAME, "i5p").find_element(
+                    By.CLASS_NAME, "tile-hover-target").find_element(By.CLASS_NAME, "de0").text
+                csv_writer.writerow([name, str(price), reviews, link])
+        except Exception as e:
+            print(f"Parsing: {e} in {category_id}")
+        try:
+            item = WebDriverWait(driver, 3).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, "sy2")))
+            item.click()
+        except TimeoutException as e:
+            break
+        except Exception as e:
+            print(f"Next button: {e} in {category_id}")
+            break
+    try:
+        csv_file.close()
+    except Exception as e:
+        print(f"Can't close file: {e} in {category_id}")
